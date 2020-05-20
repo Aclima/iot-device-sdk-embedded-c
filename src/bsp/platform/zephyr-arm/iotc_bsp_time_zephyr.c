bf1aadd53b502295e70e7711640be2ce852cf5f0
Mike Voytovich <mike@rokkresearch.com>
2020-05-20 14:58:19 -0700

    obtain wall time through my_k_time()
    Zephyr doesn't support gettimeofday() unless POSIX is
    enabled, which we causes conflicts like namespace collisions
    and link errors with our Zephyr configuration.

--- src/bsp/platform/zephyr-arm/iotc_bsp_time_zephyr.c
+++ src/bsp/platform/zephyr-arm/iotc_bsp_time_zephyr.c
@@ -21,22 +21,17 @@
 
 #include <stddef.h>
 #include <time.h>
+#include "iotc_sntp.h"
 
 void iotc_bsp_time_init() { /* empty */
 }
 
 iotc_time_t iotc_bsp_time_getcurrenttime_seconds() {
-  struct timeval current_time;
-  gettimeofday(&current_time, NULL);
-  return (iotc_time_t)((current_time.tv_sec) +
-                       (current_time.tv_usec + 500000) /
-                           1000000); /* round the microseconds to seconds */
+    /* from nRF Connect SDK app */
+    return my_k_time(NULL);
 }
 
 iotc_time_t iotc_bsp_time_getcurrenttime_milliseconds() {
-  struct timeval current_time;
-  gettimeofday(&current_time, NULL);
-  return (iotc_time_t)((current_time.tv_sec * 1000) +
-                       (current_time.tv_usec + 500) /
-                           1000); /* round the microseconds to milliseconds */
+    /* not implemented */
+    return 0;
 }
